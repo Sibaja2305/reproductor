37b45253b1d535f9f7f32981c31b7d0e063fcc74
Sibaja2305 <kevinsibajah@gmail.com>
2023-03-28 13:36:53 -0600

    nuevo

---
+++ src/sibajaplay/Cancion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sibajaplay;
+
+/**
+ *
+ * @author Hp EliteBook
+ */
+public class Cancion {
+     String autor;
+     String name;
+     
+     
+}
